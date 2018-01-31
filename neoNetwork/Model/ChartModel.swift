d20335922c9cc22c55fa27481ee5971fbcb6e895
xandersung <alexandersung11@gmail.com>
2018-01-31 03:25:13 -0800

    first commit

---
+++ neoNetwork/Model/ChartModel.swift
@@ -0,0 +1,9 @@
+//
+//  ChartModel.swift
+//  neoNetwork
+//
+//  Created by Alexander Sung on 1/30/18.
+//  Copyright © 2018 Alexander Sung. All rights reserved.
+//
+
+import Foundation
---
+++ neoNetwork/NEP5Cell.swift
@@ -0,0 +1,9 @@
+//
+//  NEP5Cell.swift
+//  neoNetwork
+//
+//  Created by Alexander Sung on 1/25/18.
+//  Copyright © 2018 Alexander Sung. All rights reserved.
+//
+
+import Foundation
